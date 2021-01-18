4f73bb218a25e4d9d61e9a73fbed10eceb091739
Mézz <ysaline.franquet@gmail.com>
2021-01-18 14:00:22 +0100

    Aside - Categories

--- app/Http/Controllers/CategorieController.php
+++ app/Http/Controllers/CategorieController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class CategorieController extends Controller
 {
-    //
+  /**
+   * [index description]
+   * @param  integer $limit [description]
+   * @return [type]         [description]
+   */
+  public function index(INT $limit = 10) {
+    $categories = Categorie::orderBy('created_at', 'desc')
+                ->take($limit)
+                ->get();
+    return view('categories.index', compact('categories'));
+  }
 }
